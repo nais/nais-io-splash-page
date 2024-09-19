fa66d55c7702fb86d3a78870b0febe6b60741f5d
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-09-19 08:38:37 +0200

    Minor tweaks

--- src/components/PrimaryFeatures.tsx
+++ src/components/PrimaryFeatures.tsx
@@ -76,10 +76,10 @@ export function PrimaryFeatures() {
       <Container className="relative">
         <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-            Alt dine team trenger for å lage applikasjoner.
+            Alt team trenger for å lage applikasjoner.
           </h2>
           <p className="mt-6 text-lg tracking-tight text-blue-100">
-            Med nais kan dine team fokusere på å lage applikasjoner, ikke administrere infrastruktur.
+            Med nais kan teamene fokusere på å lage applikasjoner, ikke administrere infrastruktur.
           </p>
         </div>
         <TabGroup
