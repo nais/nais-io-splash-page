3b48517be37caf4ad66f96c3814feb831019c0fe
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-10-15 13:35:02 +0200

    Update packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { CallToAction } from '@/components/CallToAction'
 import { Faqs } from '@/components/Faqs'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
@@ -6,7 +5,6 @@ import { Hero } from '@/components/Hero'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
 import { SecondaryFeatures } from '@/components/SecondaryFeatures'
 import { Technology } from '@/components/Technology'
-import { Testimonials } from '@/components/Testimonials'
 
 export default function Home() {
   return (
